2142430f2dcbb81de887d7e69bac606563c715fd
charles19920528 <charles19920528@hotmail.com>
2018-04-18 14:43:26 -0700

    Add files via upload

--- model_training_functions.py
+++ model_training_functions.py
@@ -79,7 +79,14 @@ def uni_prob(char_freq_dict, vocab_size, perturb_mass, beg_char):
     return prob_dict
 
 
-def bi_prob(char_freq_dict, freq_dict_bi, vocab_size, perturb_mass):
+# uni_probability_dict = uni_prob(char_freq_dict, unicode_upper + 1, 0.1, beg_char)
+#
+# # check if sum up to 1
+# sum(uni_probability_dict.values()) - uni_probability_dict["unseen"] + uni_probability_dict["unseen"] * \
+# (unicode_upper + 1 - len(uni_probability_dict) + 1)
+
+
+def bi_prob(char_freq_dict, freq_dict_bi, vocab_size, perturb_mass, beg_char):
     # Input: freq_dict_bi: contains counts of (char j, char i) etc.
     # Output: a dictionary of len(freq_dict_bi) - 1 with key (j, i) storing p(word i | word j).
 
@@ -98,7 +105,7 @@ def bi_prob(char_freq_dict, freq_dict_bi, vocab_size, perturb_mass):
     return prob_dict
 
 
-def tri_prob(freq_dict_bi, freq_dict_tri, vocab_size, perturb_mass):
+def tri_prob(freq_dict_bi, freq_dict_tri, vocab_size, perturb_mass, beg_char):
     prob_dict = {}
     total_perturb_mass = perturb_mass * vocab_size
 
@@ -133,8 +140,8 @@ def language_model(string, string_char_list, beg_char, vocab_size, perturb_mass)
 
     perturb_mass_uni, perturb_mass_bi, perturb_mass_tri = perturb_mass
     uni_probability_dict = uni_prob(char_freq_dict, vocab_size, perturb_mass_uni, beg_char)
-    bigram_probability_dict = bi_prob(char_freq_dict, bi_pair_freq, vocab_size, perturb_mass_bi)
-    trigram_probability_dict = tri_prob(bi_pair_freq, tri_pair_freq, vocab_size, perturb_mass_tri)
+    bigram_probability_dict = bi_prob(char_freq_dict, bi_pair_freq, vocab_size, perturb_mass_bi, beg_char)
+    trigram_probability_dict = tri_prob(bi_pair_freq, tri_pair_freq, vocab_size, perturb_mass_tri, beg_char)
 
     return uni_probability_dict, bigram_probability_dict, trigram_probability_dict
 
